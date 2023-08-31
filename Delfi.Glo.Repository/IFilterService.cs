989fdb37363f4ad6138bd7db09f6d2e9f9ebc584
Deendayal Rajak <deendayalr@birlasoft.com>
2023-08-31 18:02:05 +0530

    Pushed Ravindra code for wellList

---
+++ Delfi.Glo.Repository/IFilterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delfi.Glo.Repository
+{
+    public interface IFilterService<T1,T2>
+    {
+        Task<Tuple<bool, IEnumerable<T1>, int, int, int, int>> GetListByFilter(T2 item);
+    }
+}
