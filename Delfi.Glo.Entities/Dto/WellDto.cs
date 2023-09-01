7c6a26b4bd905963f732901c4f967d40b4ba3a0a
Deendayal Rajak <deendayalr@birlasoft.com>
2023-09-01 21:13:30 +0530

    Code changes ravindra-01 sep 2023

--- Delfi.Glo.Entities/Dto/WellDto.cs
+++ Delfi.Glo.Entities/Dto/WellDto.cs
@@ -6,25 +6,25 @@ using System.Threading.Tasks;
 
 namespace Delfi.Glo.Entities.Dto
 {
-    public class WellDto : DtoBaseEntity
+    public class WellDto:DtoBaseEntity
     {
-        public string WellName { get; set; }
-        public string PumpStatus { get; set; }
-        public decimal Wc { get; set; }
-        public decimal GlInjectionSetPoint { get; set; }
-        public decimal CompressorUpTime { get; set; }
-        public decimal ProductionUpTime { get; set; }
-        public decimal DeviceUpTime { get; set; }
-        public DateTime TimeStamp { get; set; }
+        public string WellName { get;  set; }
+        public string PumpStatus { get;  set; }
         public int GLISetPoint { get; set; }
-        public int OLiq { get; set; }
         public int QOil { get; set; }
-        //  public int Wc { get; private set; }
+        public int OLiq { get; set; }
+        public int Og { get; set; }
+        public int Ow { get; set; }
+        public decimal Wc { get;  set; }
+        public decimal CompressorUpTime { get;  set; }
+        public decimal ProductionUpTime { get; set; }
+        public decimal DeviceUpTime { get;  set; }
         public string LastCycleStatus { get; set; }
-        public int CurrentGLISetpoint { get; set; }
-        public string CycleStatus { get; set; }
-        public string ApprovalMode { get; set; }
-        public string ApprovalStatus { get; set; }
+        public DateTime TimeStamp { get;  set; }
+        public int CurrentGLISetpoint { get;  set; }
+        public string PreprocessorState { get; set; }
+        public string ModeOfOperation { get;  set; }
+        public string CurrentCycleStatus { get;  set; }
         public string UserId { get; set; }
         public int NoOfAlerts { get; set; }
 
